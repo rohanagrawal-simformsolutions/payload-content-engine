302e9db21f8354e01f79f9fece0d988b21806bd0
Rohan Agrawal <rohan.agrawal@simformsolutions.com>
2026-03-13 13:31:02 +0530

    refactor: update PrismaService to use generated client and improve connection handling

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,24 +1,29 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from "@nestjs/common";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../../generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
+export class PrismaService implements OnModuleInit, OnModuleDestroy {
+  // PrismaClient is exported as both a const (constructor) and a type alias
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private readonly client: any;
+
   constructor() {
     const adapter = new PrismaPg({
       connectionString: process.env.DATABASE_URL,
     });
-    super({ adapter });
+    this.client = new PrismaClient({ adapter });
+  }
+
+  get user() {
+    return this.client.user;
   }
 
   async onModuleInit() {
-    await this.$connect();
+    await this.client.$connect();
   }
 
   async onModuleDestroy() {
-    await this.$disconnect();
+    await this.client.$disconnect();
   }
 }
